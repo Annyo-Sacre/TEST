bdef00c336cfcc4d7ecc993584c71972018fef66
Annyo-Sacre <122270482+Annyo-Sacre@users.noreply.github.com>
2025-01-22 12:17:53 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Afficher un message de bienvenue
+  print('Bienvenue dans ce programme Dart simple!');
+
+  // Déclarer deux nombres
+  int nombre1 = 5;
+  int nombre2 = 10;
+
+  // Calculer leur somme
+  int somme = nombre1 + nombre2;
+
+  // Afficher le résultat
+  print('La somme de $nombre1 et $nombre2 est : $somme');
+}
